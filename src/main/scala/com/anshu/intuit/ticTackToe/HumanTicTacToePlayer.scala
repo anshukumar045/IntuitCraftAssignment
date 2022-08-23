36583e3846367a4a2c4c8e8b44da3eb327c1b75a
Kumar Anshu <52275163+anshukumar045@users.noreply.github.com>
2022-08-23 13:04:41 +0530

    Update HumanTicTacToePlayer.scala

--- src/main/scala/com/anshu/intuit/ticTackToe/HumanTicTacToePlayer.scala
+++ src/main/scala/com/anshu/intuit/ticTackToe/HumanTicTacToePlayer.scala
@@ -7,17 +7,32 @@ import scala.util.{Failure, Success, Try}
  * Player is prompted to enter move
  */
 class HumanTicTacToePlayer  extends MoveOnBoard[TicTacToeGameState] {
+  import HumanTicTacToePlayer._
   import Utility._
 
   override def move(s: TicTacToeGameState): TicTacToeGameState = {
     println(msgForInput)
-    val vertex = scala.io.StdIn.readLine().split(",")
-    val position = Position(vertex(0).toInt, vertex(1).toInt)
+    val vertex = readInputVertex
+    val position = Position(vertex(0), vertex(1))
     Try(s.makeMove(position)) match {
       case Success(value) => value
       case Failure(_: Throwable) =>
         println(invalidMove)
+        println(msgForInput)
         move(s)
+    }
+  }
+}
+
+object HumanTicTacToePlayer {
+  import Utility._
+  private def readInputVertex: Array[Int] = {
+    Try(scala.io.StdIn.readLine().split(",").map(_.toInt)) match {
+      case Success(value) if value.length == 2 => value
+      case Failure(_: Throwable) =>
+        println(invalidMove)
+        println(msgForInput)
+        readInputVertex
     }
   }
 }
